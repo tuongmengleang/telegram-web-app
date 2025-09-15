3a417630bbcd3ed7ab2db8fe22914163679ec5da
tuongmengleang <mengleangtuong.it@gmail.com>
2025-09-15 15:49:10 +0700

    🐯chore: register @telegram-apps/sdk-vue and eruda for dev

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
+import './assets/styles/main.css';
 import './assets/styles/index.css';
 import App from './App.vue';
-import {setupRouter} from './router';
-import {init} from '@telegram-apps/sdk-vue'
-import {setupEruda} from '@/plugins/eruda.ts'
+import { setupRouter } from './router';
+import { init } from './init';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-vue';
 
 const app = createApp(App);
 
 // Initialize the @telegram-apps/sdk-vue package.
-init();
+init(retrieveLaunchParams().startParam === 'debug' || import.meta.env.DEV);
 
 async function bootstrap() {
     // load plugins
-    setupEruda()
     await setupRouter(app);
     app.mount('#app');
 }
