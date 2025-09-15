ab2ef22baa8ac1861bc66a077f6fe40155587fd3
tuongmengleang <mengleangtuong.it@gmail.com>
2025-09-15 15:56:09 +0700

    👉🏼chore: display initData

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts">
-import { Button } from "@/components/ui/button"
+<script lang="ts" setup>
+import { initData, useSignal } from '@telegram-apps/sdk-vue';
+
+const initDataRef = useSignal(initData.state);
 </script>
 
 <template>
-  <Button>Button</Button>
+    <div class="p-4">
+        <h4 class="title text-2xl text-center font-medium">Index Page</h4>
+        <pre>initDataRef: {{ initDataRef }}</pre>
+    </div>
 </template>
 
 <style scoped>
-
+.title {
+    background-color: var(--tg-theme-button-color);
+    color: var(--tg-button-text-color);
+}
 </style>
